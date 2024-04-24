2a80becc4a3e2d23f1ff7cddbb2471106bcb3304
pplociennik <pplociennik097@gmail.com>
2024-04-24 22:39:09 +0200

    Initial commit.

---
+++ src/main/java/pl/esportowapolska/espserverapp/EspServerAppApplication.java
@@ -0,0 +1,13 @@
+package pl.esportowapolska.espserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspServerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/esportowapolska/espserverapp/EspServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.esportowapolska.espserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

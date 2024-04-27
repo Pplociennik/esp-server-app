3b4f09dc02774b8d2a08488073d47152216d5527
pplociennik <pplociennik097@gmail.com>
2024-04-27 02:40:14 +0200

    Added liquibase support.

---
+++ ee-server-db/src/test/java/pl/esportowapolska/db/EeServerDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.esportowapolska.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeServerDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

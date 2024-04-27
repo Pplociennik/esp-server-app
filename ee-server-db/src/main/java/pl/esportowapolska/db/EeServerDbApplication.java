d854d6bf6dd1aa274c1358161c7284bf8ad253c0
pplociennik <pplociennik097@gmail.com>
2024-04-27 02:28:36 +0200

    Added liquibase support.

---
+++ ee-server-db/src/main/java/pl/esportowapolska/db/EeServerDbApplication.java
@@ -0,0 +1,13 @@
+package pl.esportowapolska.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeServerDbApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( EeServerDbApplication.class, args );
+    }
+
+}

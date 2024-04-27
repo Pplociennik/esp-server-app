6c3109b41a9995cd7a5714b7ee66c6b6c170e343
pplociennik <pplociennik097@gmail.com>
2024-04-27 15:23:26 +0200

    Added auth config import.

--- esp-server-core/src/main/java/pl/esportowapolska/core/CoreApplication.java
+++ esp-server-core/src/main/java/pl/esportowapolska/core/CoreApplication.java
@@ -1,9 +1,12 @@
 package pl.esportowapolska.core;
 
+import com.github.pplociennik.auth.core.configuration.DefaultStaticSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import( DefaultStaticSecurityConfiguration.class )
 public class CoreApplication {
 
     public static void main( String[] args ) {
